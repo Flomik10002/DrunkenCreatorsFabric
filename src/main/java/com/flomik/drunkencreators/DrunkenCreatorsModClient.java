65965ea3c29a5549cbc00bddb9cd17415bdb020d
Flomik <79540285+Flomik10002@users.noreply.github.com>
2023-08-25 13:45:59 +0400

    Initial Commit

---
+++ src/main/java/com/flomik/drunkencreators/DrunkenCreatorsModClient.java
@@ -0,0 +1,10 @@
+package com.flomik.drunkencreators;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrunkenCreatorsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/flomik/drunkencreators/mixin/MinecraftMixin.java
@@ -0,0 +1,20 @@
+package com.flomik.drunkencreators.mixin;
+
+import com.flomik.drunkencreators.DrunkenCreatorsMod;
+
+import net.minecraft.client.Minecraft;
+
+import net.minecraft.client.main.GameConfig;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void example$init(GameConfig gameConfig, CallbackInfo ci) {
+		DrunkenCreatorsMod.LOGGER.info("Hello from {}", DrunkenCreatorsMod.NAME);
+	}
+}
