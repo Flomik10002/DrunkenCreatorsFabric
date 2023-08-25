cb473240b9bef1252d6f517662b8860184a2a750
Flomik <79540285+Flomik10002@users.noreply.github.com>
2023-08-25 13:53:42 +0400

    Initial Commit

---
+++ src/main/java/com/flomik/drunkencreators/mixin/BackgroundRendererMixin.java
@@ -0,0 +1,43 @@
+package com.flomik.drunkencreators.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.BackgroundRenderer.FogType;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.fluid.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Environment(EnvType.CLIENT)
+@Mixin(BackgroundRenderer.class)
+public abstract class BackgroundRendererMixin {
+    @Shadow
+    private static float red;
+
+    @Shadow
+    private static float green;
+
+    @Shadow
+    private static float blue;
+
+    @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;clearColor(FFFF)V", remap = false))
+    private static void drunkencreators$modifyFogColors(Args args, Camera camera, float partialTicks, ClientWorld level, int renderDistanceChunks, float bossColorModifier) {
+        FluidState state = level.getFluidState(camera.getBlockPos());
+
+    }
+
+    @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
+    private static void drunkencreators$applyFog(Camera camera, FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
+        assert MinecraftClient.getInstance().world != null;
+        FluidState state = MinecraftClient.getInstance().world.getFluidState(camera.getBlockPos());
+
+    }
+}
